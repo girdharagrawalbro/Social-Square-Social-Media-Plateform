b4d38ca9d181551b76ad190d19fcb6b2c1340022
Girdhar Agrawal <girdharagrawalbro@gmail.com>
2024-11-13 07:21:15 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Form from './pages/Form'
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Form />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
