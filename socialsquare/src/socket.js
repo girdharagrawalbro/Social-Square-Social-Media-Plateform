07cad2ee3f381945c2a0031f07e0efa82e585e84
Girdhar Agrawal <girdharagrawalbro@gmail.com>
2026-05-30 15:21:45 +0530

    fix: update environment variable references for backend URL

--- socialsquare/src/socket.js
+++ socialsquare/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const BACKEND_URL = process.env.NGINIX ? "" : process.env.REACT_APP_BACKEND_URL;
+const BACKEND_URL = process.env.REACT_APP_NGINIX ? "" : process.env.REACT_APP_BACKEND_URL;
 
 export const socket = io(BACKEND_URL, {
     withCredentials: true,
