d28026880da31a92a3701d9622396df7f6b174a2
Girdhar Agrawal <girdharagrawalbro@gmail.com>
2026-06-15 17:58:14 +0530

    feat: add EmailTemplatesTab component for managing system email templates

--- socialsquare/src/pages/components/EmailTemplatesTab.js
+++ socialsquare/src/pages/components/EmailTemplatesTab.js
@@ -27,6 +27,7 @@ export default function EmailTemplatesTab() {
 
     useEffect(() => {
         fetchTemplates();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     const handleSeed = async () => {
